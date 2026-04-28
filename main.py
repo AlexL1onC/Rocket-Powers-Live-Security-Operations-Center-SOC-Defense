70d8f0cc540513e48f729e47593c763d35cea9a0
Erick-Lascano <ericklascano99@gmail.com>
2026-04-28 02:25:32 -0600

    experimentation Unsupervised Lerning

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-# main.py
-import os
+import pandas as pd
 
-from dotenv import load_dotenv
+df = pd.read_json("data/raw_logs.json")
 
-from data_ingestion.data_extraction import download_all_pages
-from data_ingestion.processor import clean_and_convert_to_parquet
-
-load_dotenv()
-
-def run_pipeline():
-    print("Iniciando Pipeline de Defensa SOC...")
-    
-    # 1. Captura (Tu misión actual)
-    #raw_data = download_all_pages() 
-    raw_data = os.getenv('OUTPUT_FOLDER')# Ya descargaste el JSON gigante, así que vamos directo a procesar ese archivo
-
-    # 2. Procesamiento (Siguiente paso)
-    #clean_df = clean_and_convert_to_parquet(raw_data, os.getenv("OUTPUT_PARQUET_FOLDER"))
-    parquet_data = os.getenv("OUTPUT_PARQUET_FOLDER") # Ya procesaste el Parquet, así que vamos directo a ese archivo
-    # 3. Alerta (Webhook - Se activa el 27 de abril) [cite: 438]
-    # send_alert_if_needed(clean_df)
-
-if __name__ == "__main__":
-    run_pipeline()
+print(df.columns)
